5f14abd372c8b570e233a7d47ca28d5f47231d3a
rchalla67 <rishichalla67@gmail.com>
2022-08-05 20:39:13 -0400

    added profile pic view in profile view

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -34,6 +34,12 @@ export default function Profile() {
             </div>
             <div className="border-t border-gray-200">
               <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500 flex items-center justify-center">Profile Picture</dt>
+                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex items-center justify-center">
+                    <img className="h-40 w-40 " disabled={user.photo === ''} src={ user.photo } alt="" />
+                  </dd>
+                </div>
                 <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                   <dt className="text-sm font-medium text-gray-500">Full name</dt>
                   <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
