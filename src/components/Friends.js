52d838c9dabd0da9df1ece89a947338e3641358e
rchalla67 <rishichalla67@gmail.com>
2022-08-27 11:49:42 -0400

    updated mobile view of friends to add username

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -70,9 +70,19 @@ export default function Friends() {
                 <div key={user.id} className="pt-10 grid place-items-center pb-5">
                   {activeUser.friends.includes(user.username) && 
                     <div className="p-3 min-w-75% md:max-w-5xl bg-slate-100 rounded-lg border border-slate-500 shadow-lg items-center">
-                      {/* TODO: Make gradient be selected state */}
-                      <div  className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-gradient-to-r from-sky-300 via-sky-400 to-sky-500">
-                        <dt className="text-xl font-medium text-black flex items-center justify-center">{user.firstName}</dt>
+                      <div  className="sm:hidden bg-gray-50 px-4 py-5 bg-gradient-to-r from-sky-300 via-sky-400 to-sky-500">
+                        <div className="text-xl font-medium text-black flex items-center justify-center">
+                          {user.firstName}
+                        </div>
+                        <div className="align-items">{user.username}</div>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex items-center justify-center">
+                          {user.photo && <img className="h-48 w-40 rounded-lg" src={ user.photo } alt="" />}
+                        </dd>
+                      </div>
+                      <div  className="hidden bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-gradient-to-r from-sky-300 via-sky-400 to-sky-500">
+                        <div className="text-xl font-medium text-black flex items-center justify-center">
+                          {user.firstName}
+                        </div>
                         <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex items-center justify-center">
                           {user.photo && <img className="h-48 w-40 rounded-lg" src={ user.photo } alt="" />}
                         </dd>
@@ -80,11 +90,9 @@ export default function Friends() {
                       <div onClick={() => removeFriend(user.username) } className="pt-2 hover:cursor-pointer hover:bg-">
                         {activeUser.friends.includes(user.username) && <a className="inline-flex justify-center w-full py-2 px-4 border border-transparent shadow-sm text-med font-medium rounded-md text-white bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-900">
                           Remove Friend
-                        </a>}  
-                        
+                        </a>}   
                       </div>
                     </div>
-                    
                   }
                 </div>
                 )})}
@@ -100,7 +108,14 @@ export default function Friends() {
                   {(user.firstName && user.photo && user.id !== activeUser.id) && 
                     <div className="p-3 min-w-75% md:max-w-5xl bg-slate-100 rounded-lg border border-slate-500 shadow-lg items-center">
                       {/* TODO: Make gradient be selected state */}
-                      <div  className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-gradient-to-r from-sky-300 via-sky-400 to-sky-500">
+                      <div  className="sm:hidden bg-gray-50 px-4 py-5 bg-gradient-to-r from-sky-300 via-sky-400 to-sky-500">
+                        <dt className="text-xl font-medium text-black flex items-center justify-center">{user.firstName}</dt>
+                        <div className="align-items">{user.username}</div>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex items-center justify-center">
+                          {user.photo && <img className="h-48 w-40 rounded-lg" src={ user.photo } alt="" />}
+                        </dd>
+                      </div>
+                      <div  className="hidden bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-gradient-to-r from-sky-300 via-sky-400 to-sky-500">
                         <dt className="text-xl font-medium text-black flex items-center justify-center">{user.firstName}</dt>
                         <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex items-center justify-center">
                           {user.photo && <img className="h-48 w-40 rounded-lg" src={ user.photo } alt="" />}
@@ -109,17 +124,15 @@ export default function Friends() {
                       <div onClick={() => addFriend(user.id, user.username) } className="pt-2 hover:cursor-pointer hover:bg-">
                         {!activeUser.friends.includes(user.username) && <a className="inline-flex justify-center w-full py-2 px-4 border border-transparent shadow-sm text-med font-medium rounded-md text-white bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-900">
                           Add Friend
-                        </a>}  
-                        
+                        </a>}    
                       </div>
                       <div onClick={() => removeFriend(user.username) } className="pt-2 hover:cursor-pointer hover:bg-">
                         {activeUser.friends.includes(user.username) && <a className="inline-flex justify-center w-full py-2 px-4 border border-transparent shadow-sm text-med font-medium rounded-md text-white bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-900">
                           Remove Friend
                         </a>}  
-                        
+ 
                       </div>
                     </div>
-                    
                   }
                 </div>
                 )})}
